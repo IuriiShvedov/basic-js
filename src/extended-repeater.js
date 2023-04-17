7e0c1ad1c70cb7f2a4176266ec86422092847912
IuriiShvedov <yuryshvd@gmail.com>
2023-04-17 21:53:11 +0400

    feat: dinished couple tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  
+function repeater(str, options) {
+  let string = String(str),
+    repeatTimes = options.repeatTimes || 1,
+    separator = options.separator || '+',
+    additionRepeatTimes = options.additionRepeatTimes || 1,
+    additionSeparator = String(options.additionSeparator || '|');
+    let addition = String(options.addition),
+    result = '';
+    if(addition === 'undefined') {
+      addition = ''}else {
+        addition = addition
+      }
+      for (let i = 0; i < repeatTimes; i++) {
+        result += string;
+        for (let n = 0; n < additionRepeatTimes; n++) {
+          result += addition;
+          if (n != additionRepeatTimes - 1) {
+            result += additionSeparator;
+          }
+        }
+        if (i !== repeatTimes - 1) {
+          result += separator;
+        }
+      }
+      return result;
 }
 
 module.exports = {
